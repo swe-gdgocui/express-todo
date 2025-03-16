ff3614b5d98d52afc8cd66486a6d6dabc2e246f5
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-03-16 16:42:53 +0700

    feat: prisma and vercel

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Import Express
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const app = express();
 
 app.use(express.json());
@@ -10,21 +13,66 @@ app.use((req, res, next) => {
   next(); // Pass the request to the next middleware or route
 });
 
-let todos = [
-  { id: 1, task: 'Buy groceries' },
-  { id: 2, task: 'Clean the house' }
-];
-
 // Get all todos
-app.get('/todos', (req, res) => {
-  res.json(todos);
+app.get('/todos', async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  res.json({
+    message: 'Todos retrieved',
+    todos,
+  });
 });
 
 // Create a new todo
-app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, task: req.body.task };
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
+app.post('/todos', async (req, res) => {
+  const newTodo = { 
+    title: req.body.title,
+    description: req.body.description,
+    completed: req.body.completed || false,
+  };
+
+  if (!newTodo.title) {
+    return res.status(400).json({ error: 'Title is required' });
+  }
+
+
+  const todo = await prisma.todo.create({
+    data: {
+      title: newTodo.title,
+      completed: newTodo.completed,
+      description: newTodo.description,
+    }
+  });
+
+  res.status(201).json({
+    message: 'Todo created',
+    todo
+  });
+});
+
+app.put('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  const todo = await prisma.todo.update({
+    where: { id: parseInt(id) },
+    data: {
+      title: req.body.title,
+      description: req.body.description,
+      completed: req.body.completed,
+    }
+  });
+
+  res.json({
+    message: 'Todo updated',
+    todo,
+  });
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  await prisma.todo.delete({
+    where: { id: parseInt(id) }
+  });
+
+  res.json({ message: 'Todo deleted' });
 });
 
 // Start server
